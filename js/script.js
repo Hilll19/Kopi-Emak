1430e6b350a529e023ad061ff810ab16e677c36f
Hilll19 <hilworking0110@gmail.com>
2023-03-15 23:42:06 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ const navbarNav = document.querySelector('.navbar-nav');
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
+
+// When click out sidebar this for undisplay nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.targe)&& !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+    // when user click out from e target its humburgermenu and navbar will be non active
+});
